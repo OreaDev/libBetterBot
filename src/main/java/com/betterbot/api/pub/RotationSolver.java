ad361a3e355d012c3fb66c4badead562d37d63c0
OreaDev <30421461+OreaDev@users.noreply.github.com>
2017-09-05 09:31:48 -0700

    Update RotationSolver.java

--- src/main/java/com/betterbot/api/pub/RotationSolver.java
+++ src/main/java/com/betterbot/api/pub/RotationSolver.java
@@ -11,4 +11,5 @@ public interface RotationSolver {
     public void pull(Unit u); // called when the bot is not in combat and is trying to pull the unit
     public boolean combatEnd(Unit u); // return true if the bot should still wait before walking (Ex the solver is drinking)
     public int getPullDistance(Unit u); // get the max distance the bot can be before pulling the unit
+    public void approaching(Unit u); // called when the bot is approeaching the target (feel free to cast spells here)
 }
