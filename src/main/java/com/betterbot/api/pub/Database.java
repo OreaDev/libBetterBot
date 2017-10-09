491fddcf16c4cc444f46a62b286ecc4424ace909
OreaDev <30421461+OreaDev@users.noreply.github.com>
2017-10-09 15:51:02 -0700

    API additions (r325+)

--- src/main/java/com/betterbot/api/pub/Database.java
+++ src/main/java/com/betterbot/api/pub/Database.java
@@ -18,6 +18,12 @@ public interface Database {
         public String name;
     }
     
+    public static class Vendor {
+        public Vector3f position;
+        public String name;
+        public boolean food, ammo, repair, poison, reagent;
+    }
+    
     public ArrayList<Vector3f> getSpawns(Type t, int ... ids);
     public ArrayList<Vector3f> getSpawns(Type t, String ... names);
     
@@ -38,5 +44,12 @@ public interface Database {
     * @return the flight ID (used by Movement.enableFlightPaths)
     */
     public int getFlightIDForName(String name);
+    
+    public Vendor getNearestVendor();
+    public Vendor getNearestRepair();
+    public Vendor getNearestPoisons();
+    public Vendor getNearestReagents();
+    public Vendor getNearestFoodAndWater();
+    public Vendor getNearestAmmo();
     
 }
